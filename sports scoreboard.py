0870f87ebc082209752ad1cfb1e3e7d36925633b
erwanfanya <fanyaerwan@gmail.com>
2026-09-16 18:23:27 -0400

    Add files via upload

---
+++ sports scoreboard.py
@@ -0,0 +1,62 @@
+class Cricket:
+    def __init__(self, player, score):
+        self.__player = player
+        self.__score = score
+
+    def info(self):
+        print(f"cricket - player: {self.__player}, score: {self.__score}")
+
+    def play(self):
+        print(f"{self.__player} hit a six")
+
+    def get_score(self):
+        return self.__score
+
+    def set_score(self, new_score):
+        if new_score >= 0:
+            self.__score = new_score
+            print(f"Score update to {self.__score}")
+
+        else:
+            print("Score cannot be negative")
+
+class Football:
+    def __init__(self,player,score):
+        self.__player = player
+        self.__score = score
+
+    def info(self):
+        print(f"Football - player: {self.__player}, Score: {self.__score}")
+
+    def play(self):
+        print(f"{self.__player} score a goal")
+
+    def get_score(self):
+        return self.__score
+
+    def set_score(self, new_score):
+        if new_score >= 0:
+            self.__score = new_score
+            print(f"Score update to {self.__score}")
+        else:
+            print("score canno be negative ")
+
+cricket = Cricket("Rohit", 85)
+football = Football("Erwan", 2)
+
+print("=== Sport Scoreboard === \n")
+for sport in (cricket, football ):
+    sport.info()
+    sport.play()
+    print()
+
+print("--- Direct change attempt ---")    
+cricket.__score = 999
+print(f"get_score() still shoes: {cricket.get_score()}")
+
+print("\n--- updating score ---")
+cricket.set_score(100)
+football.set_score(3)
+
+
+
